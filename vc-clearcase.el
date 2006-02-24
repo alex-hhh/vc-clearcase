6b0af1138a2e6402e76ac03eb745b5b6882ea221
haral <haral@localhost>
2006-02-24 20:25:50 +0800

    1.68 applied GPL licence to the file, added the sourceforge address for the vc-clearcase project

--- vc-clearcase.el
+++ vc-clearcase.el
@@ -1,8 +1,26 @@
 ;; vc-clearcase.el --- support for ClearCase version control system
+;;
+;;     Copyright (C) 2006 Alexandru Harsanyi
+;;
+;;     This program is free software; you can redistribute it and/or modify
+;;     it under the terms of the GNU General Public License as published by
+;;     the Free Software Foundation; either version 2 of the License, or
+;;     (at your option) any later version.
+;;
+;;     This program is distributed in the hope that it will be useful,
+;;     but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;     GNU General Public License for more details.
+;;
+;;     You should have received a copy of the GNU General Public License
+;;     along with this program; if not, write to the Free Software
+;;     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+;;
 
-;; Author: Alexandru Harsanyi (harsanyi@mac.com)
+;; Author: Alexandru Harsanyi (haral@users.sourceforge.net)
 ;; Created: 28 July 2004
 ;; Keywords: version-control, clearcase
+;; Homepage: http://sourceforge.net/projects/vc-clearcase/
 ;; $Id$
 
 ;;; Commentary:
@@ -293,7 +311,7 @@
 (defconst vc-clearcase-cvsid
   "$Id$")
 
-(defconst vc-clearcase-maintainer-address "harsanyi@mac.com")
+(defconst vc-clearcase-maintainer-address "haral@users.sourceforge.net")
 
 (defgroup vc-clearcase nil
   "Support for the ClearCase version control system."

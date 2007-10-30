29dc576852ea349353071a28b2ea447eabe2d9ab
haral <haral@localhost>
2007-10-30 19:09:10 +0900

    *** empty log message ***

--- admin/install.el
+++ admin/install.el
@@ -1,61 +1,28 @@
-;; The code below will byte-compile vc-clearcase.el and generate the
-;; autoloads in a file named vc-clearcase-auto.el in the same
-;; directory as vc-clearcase.el. If the user does not have
-;; vc-clearcase.el opened, or has multiple copies of it, we ask for
-;; the file name explicitely.
-;;
-;; After the code below runs, the compilation results will be in a
-;; buffer named "*Compile-Log* (which you can switch to).  The
-;; vc-clearcase-auto.el file will be saved.
-;;
-;; To evaluate the code below, move the cursor at the end of it and
-;; type C-x C-e
+;;;; emacs -batch -l install.el -f clearcase-build-package
 
 
-;; Emacs 22 version
-(save-window-excursion
-  (require 'autoload)
-  (with-current-buffer
-      (or (get-buffer "vc-clearcase.el")
-          (find-file
-           (read-file-name "Find vc-clearcase.el: " nil nil 'must-match)))
-    (let* ((file (buffer-file-name (current-buffer)))
-           (base (file-name-nondirectory file))
-           (dir (file-name-directory file))
-           (generated-autoload-file
-            (expand-file-name "vc-clearcase-auto.el" dir)))
-      (unless (equal base "vc-clearcase.el")
-        (error "Expecting a file named vc-clearcase.el, got %s" base))
-      ;; byte-compile-file returns nil if there were errors
-      (unless (byte-compile-file file)
-        (error
-         "Failed to compile %s, check the *Compile-Log* buffer for errors"
-         file))
-      ;; update-file-autoloads returns nil if no autoloads were found.
-      (when (update-file-autoloads file 'save-after)
-        (error "Failed to find autoloads in %s" file)))))
+(require 'autoload)
 
-;; Emacs 21 version
-(save-window-excursion
-  (require 'autoload)
-  (with-current-buffer
-      (or (get-buffer "vc-clearcase.el")
-          (find-file
-           (read-file-name "Find vc-clearcase.el: " nil nil 'must-match)))
-    (let* ((file (buffer-file-name (current-buffer)))
-           (base (file-name-nondirectory file))
-           (dir (file-name-directory file))
-           (generated-autoload-file
-            (expand-file-name "vc-clearcase-auto.el" dir)))
-      (unless (equal base "vc-clearcase.el")
-        (error "Expecting a file named vc-clearcase.el, got %s" base))
-      ;; byte-compile-file returns nil if there were errors
-      (unless (byte-compile-file file)
-        (error
-         "Failed to compile %s, check the *Compile-Log* buffer for errors"
-         file))
-      (with-current-buffer (find-file generated-autoload-file)
-        (erase-buffer)
-        (insert "\f")
-        (generate-file-autoloads file)
-        (save-buffer)))))
+(defconst clearcase-package-files
+  '("vc-clearcase.el" "ucm.el"))
+
+(defconst clearcase-autoload-file "vc-clearcase-auto.el")
+
+(defun clearcase-build-package (&optional dir)
+  (unless dir (setq dir default-directory))
+  (let ((generated-autoload-file
+	 (expand-file-name clearcase-autoload-file dir))
+	(load-path (cons dir load-path)))
+    (with-current-buffer (find-file generated-autoload-file)
+      (erase-buffer)
+      (insert "\f")
+      (dolist (file clearcase-package-files)
+	(let ((file (expand-file-name file dir)))
+	  (unless (file-exists-p file)
+	    (error "cannot find %s" file))
+	  (unless (byte-compile-file file)
+	    (error
+	     "Failed to compile %s, check the *Compile-Log* buffer for errors"
+	     file))
+	  (generate-file-autoloads file)))
+      (save-buffer))))

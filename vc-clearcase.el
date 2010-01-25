4d1e27da6e33dd9ea6c5574d8f49c2bcfa9cd9fa
Alex Harsanyi <AlexHarsanyi@gmail.com>
2010-01-25 21:32:06 +0800

    Updated license to GPL3

--- vc-clearcase.el
+++ vc-clearcase.el
@@ -1,25 +1,20 @@
 ;; vc-clearcase.el --- support for ClearCase version control system
-;;
-;; NOTE: this version will not work with Emacs releases prior to version 23.
-;;
-;; Copyright (C) 2006, 2007, 2008, 2009 Alexandru Harsanyi
-;;
-;; This program is free software; you can redistribute it and/or modify
+;; Copyright (C) 2006,2007,2008,2009, 2010 Alex Harsanyi
+
+;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 2 of the License, or
+;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-;;
+
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with this program; if not, write to the Free Software
-;; Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-;;
 
-;; Author: Alexandru Harsanyi (AlexHarsanyi@gmail.com)
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;; Author: Alex Harsanyi (AlexHarsanyi@gmail.com)
 ;; Created: 28 July 2004
 ;; Keywords: version-control, clearcase
 ;; Homepage: http://code.google.com/p/vc-clearcase/
@@ -38,6 +33,7 @@
 
 ;;;;; Installation:
 ;;
+;; NOTE: this version will not work with Emacs releases prior to version 23.
 ;;
 ;; 1/ Copy this directory under the site-lisp directory of your GNU/Emacs
 ;; installation.

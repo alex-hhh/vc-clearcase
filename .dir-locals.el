dd0432280d5305b6e87904db070386dabacf4cc0
Alex Harsanyi <AlexHarsanyi@gmail.com>
2010-02-03 11:52:52 +0800

    Disallow byte-compiling of .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,5 @@
+;; -*- no-byte-compile: t -*-
+
 ;; Keep the sourceforge address until we have some bugs to link to on the new
 ;; Google Code page
 ((nil . ((bug-reference-url-format . "http://sf.net/tracker/?func=detail&aid=%s&group_id=160898&atid=817687"))))

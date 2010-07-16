3eb4938ffe7ef3d7230d79ad1eda8c75d52ab786
Alex Harsanyi <AlexHarsanyi@gmail.com>
2010-07-16 08:30:35 +0800

    Updated NEWS for release 3.4

--- vc-clearcase.el
+++ vc-clearcase.el
@@ -73,7 +73,7 @@
   (require 'trace) ; avoid compiler complaint w.r.t undefined untrace-function
   )
 
-(defconst vc-clearcase-version "3.4-pre")
+(defconst vc-clearcase-version "3.4")
 
 (defconst vc-clearcase-maintainer-address "AlexHarsanyi@gmail.com")
 
